338a6560c0ef0ce03a26ffac4217f0e733274628
sls1005 <sls1005@github.com>
2024-02-27 18:45:38 +0800

    Now use ref seq and ref string as a workaround to the scoping issue. `addr` is currently not used.

--- src/moveiterators.nim
+++ src/moveiterators.nim
@@ -19,8 +19,8 @@ iterator itemsBorrowedFrom*[T](s: var seq[T]): T {.inline.} =
   ## Yields every item from `s`.
   ## Due to safety concerns, the memory of `s` is kept out of reach during the iterations;
   ## anyone trying to access `s` will instead get an empty `seq`, and the attempt to access any of its elements could instead cause an `IndexDefect`;
-  ## this lasts until the last iteration is finished, and after that, `s` will be restored.
-  ## Any attempt to modify `s` (including trying to `add` or assign to it) within the loop, except by using an address that's taken from an element before the first iteration, will not have an effect that lasts after the end of the last iteration.
+  ## this lasts until the end of the last iteration, and after that, `s` will be restored.
+  ## Any attempt to modify `s` (including trying to `add` or assign to it) within the loop, except by using an address taken from one of the elements before the first iteration, will not have an effect that lasts after the end of the last iteration.
   ##
   ## Despite its name, this is unrelated with view types. The current implementation uses move semantics internally.
   var tmp = move(s)
@@ -32,15 +32,16 @@ iterator mutableItemsBorrowedFrom*[T](s: var seq[T]): var T {.inline.} =
   ## Yields every item from `s`.
   ## Due to safety concerns, the memory of `s` is kept out of reach (except by using the yielded variable) during the iterations;
   ## anyone trying to access `s` will instead get an empty `seq`, and the attempt to access any of its elements could instead cause an `IndexDefect`;
-  ## this lasts until the last iteration is finished, and after that, `s` will be restored.
-  ## Any attempt to modify `s` (including trying to `add` or assign to it) within the loop (except by using the yielded variable or an address that's taken from an element before the first iteration or from a yielded variable) will not have an effect that lasts after the end of the last iteration.
-  ## The current implementation uses `addr`.
+  ## this lasts until the end of the last iteration, and after that, this restriction will be undone.
+  ## Any attempt to modify `s` (including trying to `add` or assign to it) within the loop (except by using the yielded variable or an address taken from one of the elements before the first iteration or from a yielded variable) will not have an effect that lasts after the end of the last iteration.
   ##
   ## Despite its name, this is unrelated with view types. The current implementation uses move semantics internally.
-  var tmp = move(s)
-  for i in countUpBy1(0, len(tmp) - 1):
-    yield addr(tmp[i])[] # It doesn't compile otherwise.
-  s = move(tmp)
+  var r: ref seq[T] # The only reason for using `ref seq` here is that the compiler will refuse to compile if `seq` is used instead.
+  r.new()
+  r[] = move(s)
+  for i in countUpBy1(0, len(r[]) - 1):
+    yield r[][i]
+  s = move(r[])
 
 iterator itemsBorrowedFrom*(s: var string): char {.inline.} =
   var tmp = move(s)
@@ -49,13 +50,10 @@ iterator itemsBorrowedFrom*(s: var string): char {.inline.} =
   s = move(tmp)
 
 iterator mutableItemsBorrowedFrom*(s: var string): var char {.inline.} =
-  ## The current implementation uses `addr`.
-  var tmp = move(s)
-  when declared(prepareMutation):
-    prepareMutation(tmp)
-  else:
-    tmp.add("") # Ensure that it's on the heap.
-  for i in countupBy1(0,  len(tmp) - 1):
-    yield addr(tmp[i])[]
-  s = move(tmp)
+  var r: ref string # The only reason for using `ref string` here is that the compiler will refuse to compile if `string` is used instead.
+  r.new()
+  r[] = move(s)
+  for i in countupBy1(0,  len(r[]) - 1):
+    yield r[][i]
+  s = move(r[])
 
